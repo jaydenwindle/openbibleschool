ad163f26e6e0a084de439fffd7fdf27df5861116
Jayden Windle <jaydenwindle@gmail.com>
2019-02-07 17:44:25 -0800

    bootstrapped backend

---
+++ backend/learning/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=250)
+    bio = models.TextField()
+
+
+class Course(models.Model):
+    authors = models.ManyToManyField(
+        Author,
+        related_name="courses"
+    )
+
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(max_length=500)
+    index = models.PositiveIntegerField()
+    description = models.TextField()
+    url = models.URLField()
+
+
+class Video(models.Model):
+    video_id = models.CharField(max_length=250)
+    url = models.URLField()
+    lesson = models.OneToOneField(
+        Lesson,
+        on_delete=models.CASCADE,
+    )
