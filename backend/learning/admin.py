c932438be80c57ab9d02827f99852acec8212d5f
Jayden Windle <jaydenwindle@gmail.com>
2019-02-24 12:22:50 -0800

    deploy

--- backend/learning/admin.py
+++ backend/learning/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from learning.models import Author, Course, Lesson, Video, Audio
 
-# Register your models here.
 admin.site.register(Author, admin.ModelAdmin)
 admin.site.register(Course, admin.ModelAdmin)
 admin.site.register(Lesson, admin.ModelAdmin)
